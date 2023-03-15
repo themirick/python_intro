854bb620e57fda5e803378b631ab1216ba3c6b35
Mirsoat <themirick@gmail.com>
2023-03-15 23:44:48 +0500

    Done task #14

---
+++ seminars/sem2/task14.py
@@ -0,0 +1,32 @@
+"""
+Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+10 -> 1 2 4 8
+"""
+
+
+def two_degree_counter(number):
+    result = [1]
+
+    for i in range(2, number):
+        flag = True
+        temp = i
+
+        while flag:
+            if i <= 0:
+                flag = False
+            elif i == 2:
+                flag = False
+            elif i % 2 == 0:
+                i //= 2
+                continue
+            else:
+                flag = False
+
+        if i == 0 or i == 2:
+            result.append(temp)
+
+    print(f"{number} >>> {result}")
+
+
+two_degree_counter(500)
