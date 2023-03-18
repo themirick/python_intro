c12bae3807596161aaf67d1a3f2a1cf6207443fa
Mirsoat <themirick@gmail.com>
2023-03-18 10:28:52 +0500

    Edited task #23

--- seminars/sem3/task23.py
+++ seminars/sem3/task23.py
@@ -29,3 +29,15 @@ def big_counter(list):
 
 
 big_counter(l)
+
+
+# source = [0, -1, 5, 2, 3, 5]
+#
+# count = 0
+# idx = 0
+# while idx < len(source) - 1:
+#     idx += 1
+#     if source[idx - 1] < source[idx]:
+#         count += 1
+#
+# print(count)
