49eb90aedee2c2f78c580c34fcd34537f369e8c4
Mirsoat <themirick@gmail.com>
2023-03-18 09:59:04 +0500

    Edited additional task #2

--- seminars/sem3/additional2.py
+++ seminars/sem3/additional2.py
@@ -7,12 +7,12 @@
 
 
 str1 = 'урок'
-str2 = 'укор'
+str2 = 'УК ОР'
 
 
 def anagram_detector(str1, str2):
-    str1 = {i for i in str1}
-    str2 = {i for i in str2}
+    str1 = {i for i in str1.lower().replace(' ', '')}
+    str2 = {i for i in str2.lower().replace(' ', '')}
 
     if str1 == str1.intersection(str2):
         print(f"Слова являются анаграммами")
