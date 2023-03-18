a8066b7b7889897e733de9b31fe4142358441cef
Mirsoat <themirick@gmail.com>
2023-03-18 10:28:15 +0500

    Edited task #21

--- seminars/sem3/task21.py
+++ seminars/sem3/task21.py
@@ -27,3 +27,31 @@ def unic_printer(dictionary):
 
 
 unic_printer(d)
+
+
+
+
+
+
+
+# .strip()
+
+# source = [
+#     {"V": "S001"},
+#     {"V": "S002"},
+#     {"VI": "S001"},
+#     {"VI": "S005"},
+#     {"VII": "S005 "},
+#     {"V": "S009"},
+#     {"VIII": "S007"}
+# ]
+#
+#
+# def unic_printer(list_data):
+#     result = set()
+#     for item in list_data:
+#         result.add(*item.values())
+#     print(result)
+#
+#
+# unic_printer(source)
